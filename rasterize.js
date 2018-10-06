78d6b50a5cf9963fdacf88aab9a3276eb16a5965
Robert Montgomery <robm4958@gmail.com>
2018-10-05 22:18:29 -0400

    test

--- rasterize.js
+++ rasterize.js
@@ -4,8 +4,8 @@
 const WIN_Z = 0;  // default graphics window z coord in world space
 const WIN_LEFT = 0; const WIN_RIGHT = 1;  // default left and right x coords in world space
 const WIN_BOTTOM = 0; const WIN_TOP = 1;  // default top and bottom y coords in world space
-const INPUT_TRIANGLES_URL = "https://rjmontgo.github.io/prog2/triangles.json"; // triangles file loc
-const INPUT_SPHERES_URL = "https://rjmontgo.github.io/prog2/spheres.json"; // spheres file loc
+const INPUT_TRIANGLES_URL = "https://ncsucgclass.github.io/prog2/triangles.json"; // triangles file loc
+const INPUT_SPHERES_URL = "https://ncsucgclass.github.io/prog2/triangles.json"; // spheres file loc
 var Eye = new vec4.fromValues(0.5,0.5,-0.5,1.0); // default eye position in world space
 
 /* webgl globals */
@@ -80,7 +80,7 @@ function loadTriangles() {
         var triArray = []; // 1D array of triangle vertex indices
         var colors = [] // array to hold all the colors
         numVerts = 0;
-        
+
         for (var whichSet=0; whichSet<inputTriangles.length; whichSet++) {
 
             // set up the vertex coord array
